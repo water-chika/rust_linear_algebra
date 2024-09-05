defa5c05af85c440c18e74bdd1608570eef15249
Water Chika <water_chika@outlook.com>
2024-09-05 14:52:19 +0800

    Add vector prototype impl.

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+struct Vector<T,const SIZE : usize> {
+    elements : [T; SIZE]
+}
+
+impl<T: Copy, const SIZE : usize> Copy for Vector<T, SIZE> {}
+
+impl<T: Clone, const SIZE : usize> Clone for Vector<T, SIZE> {
+    fn clone(&self) -> Self {
+        Self{elements : self.elements.clone()}
+    }
+}
+
+impl<T: std::fmt::Display, const SIZE : usize> std::fmt::Display for Vector<T, SIZE> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        '('.fmt(f)?;
+        for index in 0..(SIZE-1) {
+            self.elements[index].fmt(f)?;
+            ", ".fmt(f)?;
+        }
+        if SIZE > 0 {
+            self.elements[SIZE-1].fmt(f)?;
+        }
+        ')'.fmt(f)?;
+        Ok(())
+    }
+}
+
+impl<T, const SIZE : usize> std::ops::Add for Vector<T, SIZE>
+where for<'a> &'a T: std::ops::Add<&'a T, Output=T>{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut output: Vector<T, SIZE> = self;
+        for index in 0..SIZE {
+            output.elements[index] = &output.elements[index] + &rhs.elements[index]
+        }
+        output
+    }
+}
+
+fn main() {
+    let a = Vector{ elements: [1.0, 0.5] };
+    let b = Vector{elements: [0.5, 1.5]};
+    println!("{a} + {b} = {}", a+b);
+    println!("Hello, world!");
+}
